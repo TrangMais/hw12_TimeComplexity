672422df9d54a45e4d984ecded030fb8cd2cf6dc
TrangMais <80335608+TrangMais@users.noreply.github.com>
2024-04-30 16:02:11 +0200

    Add files via upload

--- be6_day13_TO/LongestSamePrefix.java
+++ be6_day13_TO/LongestSamePrefix.java
@@ -3,16 +3,30 @@ package be6_day13_TO;
 //Time Complexity = ???? with n: the length of the input string and m: the length of prefix 
 
 public class LongestSamePrefix {
+	public static String smallestString(String[] str) {
+        //Keep track of the shortest word by index and length
+        int index = 0, minLength = str[0].length();
+        for (int i = 1; i < str.length; i++){
+            //if the next string is smaller in length then we save that index and length in our variables
+            if(str[i].length() < minLength){
+                index = i;
+                minLength = str[i].length();  
+            }           
+        }
+        //returns the smallest word
+        return str[index];
+    }
+	
 	public static String findLongestSamePrefix(String[] str){
 		if (str == null || str.length == 0) {
             return " ";
         }
         
-        String prefix = smallest(str); 									// Initialize the prefix with the first string
+        String prefix = smallestString(str); 									// Initialize the prefix with the smallest string
         
         for (int i = 1; i < str.length; i++) {
             while (str[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1); 	// Reduce the prefix by one character
+                prefix = prefix.substring(0, prefix.length() - 1); 				// Reduce the prefix by one character
                 if (prefix.isEmpty()) {
                     return " ";
                 }
@@ -33,17 +47,4 @@ public class LongestSamePrefix {
         System.out.println("Longest Same Prefix is: " + result2);
 	}
 	
-	public static String smallest(String[] str) {
-        //Keep track of the shortest word by index and length
-        int index = 0, minLength = str[0].length();
-        for (int i = 1; i < str.length; i++){
-            //if the next string is smaller in length then we save that index and length in our variables
-            if(str[i].length() < minLength){
-                index = i;
-                minLength = str[i].length();  
-            }           
-        }
-        //returns the smallest word
-        return str[index];
-    }
 }
